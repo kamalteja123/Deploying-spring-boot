8c8fd60909f95e818b4f30837052dce64793d780
Your Name <you@example.com>
2024-05-29 12:23:11 +0530

    first commit

---
+++ src/main/java/com/deploying/demo/Controller/MainController.java
@@ -0,0 +1,18 @@
+package com.deploying.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String main1()
+    {
+        return "Hello World";
+    }
+    @GetMapping("/home")
+    public String home()
+    {
+        return "welcome home";
+    }
+}
