6bd60f11a66c2d0475113d027a4b772b768c7797
Your Name <you@example.com>
2024-06-07 16:13:10 +0530

    small change

--- src/main/java/com/deploying/demo/Controller/MainController.java
+++ src/main/java/com/deploying/demo/Controller/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
     @GetMapping("/")
     public String main1()
     {
-        return "Hello World it is working";
+        return "Hello World it is working fine";
     }
     @GetMapping("/home")
     public String home()
